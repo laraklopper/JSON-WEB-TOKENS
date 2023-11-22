1317f75ac1c3ae45d2f5c2074c8edc873917ea3d
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-22 15:22:08 +0200

    Update index2.js

--- notes/index2.js
+++ notes/index2.js
@@ -1,14 +1,18 @@
-const express = require('express');// Import Express Web framework for handling HTTP requests
-const jwt = require('jsonwebtoken');// JSON Web Token library
-const bodyParser = require('body-parser');// Middleware for parsing request bodies
-const app = express();// Create an Express application
-const port = 8000;// Define the port for the server to listen on
+// Import required modules
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-//==========Middleware setup================
-// Configuring middleware to parse JSON data
+// Create an instance of the Express application
+const app = express();
+
+// Set the port for the server to listen on
+const port = 8000;
+
+// Use bodyParser middleware to parse JSON requests
 app.use(bodyParser.json());
 
-//======DATA CONTAINING USER INFORMATION============
+// Sample data representing users and their access rights
 let data = [
     {
         name: 'Mazvita',
@@ -24,90 +28,109 @@ let data = [
     }
 ]
 
-//================HANDLE POST REQUESTS TO THE '/login' ENDPOINT===========
-// Handling POST requests to the /login endpoint
+// Endpoint for user login
 app.post('/login', (req, res) => {
-    // Extracting the token from the request headers
+    // Extract the authorization header from the request
     const auth = req.headers['authorization'];
+    // The token is assumed to be directly provided in the authorization header
     const token = auth;
 
-    // Verifying if the provided token corresponds to a user in the data array
+    // Check if the provided token is valid and corresponds to a user in the data array
     const user = data.find(user => jwt.verify(token, 'jwt-secret').name === user.name);
 
     if (user) {
-        // If the user is found, create a new token with additional payload
+        // Use the user object found above to create the payload
         const payload = {
             name: user.name,
             admin: true
         }
+        // Generate a new JWT token with the payload
         const token = jwt.sign(payload, 'jwt-secret', { algorithm: 'HS256' });
+        // Send the new token in the response
         res.send({ 'token': token });
     } else {
-        // If the user is not found, return a 403 Forbidden status with an error message
+        // If the token is not valid or does not correspond to a user, return an error
         res.status(403).send({ 'err': 'incorrect login!' });
     }
 });
 
-// ============= PROTECTED ROUTES ======================
-
-// Authorization endpoints for /a, /b, and /c
+// Endpoint for accessing resource '/a'
 app.get('/a', (req, res) => {
-    // ... (similar structure for /b and /c)
+    // Extract the authorization header from the request
     const auth = req.headers['authorization'];
+    // The token is assumed to be directly provided in the authorization header
     const token = auth;
 
     // Use the token to find the corresponding user in the data array
     const user = data.find(user => jwt.verify(token, 'jwt-secret').name === user.name);
 
     if (user && (user.name === 'Mazvita' || user.name === 'Meagan')) {
+        // If the user is authorized, create a new token with an updated payload
         const payload = {
             name: user.name,
             admin: true
         }
+        // Generate a new JWT token with the updated payload
         const token = jwt.sign(payload, 'jwt-secret', { algorithm: 'HS256' });
+        // Send the new token in the response
         res.send({ 'token': token });
     } else {
+        // If the user is not authorized, return an access denied error
         res.status(403).json({ error: 'access denied' });
     }
 });
 
-// Route accessible to users with the name 'Meagan' or 'Kabelo'
+// Endpoint for accessing resource '/b'
 app.get('/b', (req, res) => {
+    // Extract the authorization header from the request
     const auth = req.headers['authorization'];
+    // The token is assumed to be directly provided in the authorization header
     const token = auth;
 
+    // Use the token to find the corresponding user in the data array
     const user = data.find(user => jwt.verify(token, 'jwt-secret').name === user.name);
 
     if (user && (user.name === 'Meagan' || user.name === 'Kabelo')) {
+        // If the user is authorized, create a new token with an updated payload
         const payload = {
             name: user.name,
             admin: true
         }
+        // Generate a new JWT token with the updated payload
         const token = jwt.sign(payload, 'jwt-secret', { algorithm: 'HS256' });
+        // Send the new token in the response
         res.send({ 'token': token });
     } else {
+        // If the user is not authorized, return an access denied error
         res.status(403).json({ error: 'access denied' });
     }
 });
 
+// Endpoint for accessing resource '/c'
 app.get('/c', (req, res) => {
+    // Extract the authorization header from the request
     const auth = req.headers['authorization'];
+    // The token is assumed to be directly provided in the authorization header
     const token = auth;
 
+    // Use the token to find the corresponding user in the data array
     const user = data.find(user => jwt.verify(token, 'jwt-secret').name === user.name);
 
     if (user && user.name === 'Kabelo') {
+        // If the user is authorized, create a new token with an updated payload
         const payload = {
             name: user.name,
             admin: true
         }
+        // Generate a new JWT token with the updated payload
         const token = jwt.sign(payload, 'jwt-secret', { algorithm: 'HS256' });
+        // Send the new token in the response
         res.send({ 'token': token });
     } else {
+        // If the user is not authorized, return an access denied error
         res.status(403).json({ error: 'access denied' });
     }
 });
 
-===================START THE SERVER===================
 // Start the server and listen on the specified port
 app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
