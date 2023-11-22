ed2a29761d1a4c9ebd3674190dfd1bf56c6387a3
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-22 17:27:17 +0200

    Update app.js

--- notes/example/app.js
+++ notes/example/app.js
@@ -5,106 +5,140 @@ const app = express();// Create an Express application
 const port = 8000;// Define the port for the server to listen on
 
 //==========Middleware setup================
-app.use(bodyParser.json());
+app.use(bodyParser.json);//bodyParser for parsing request bodies
 
 // Data containing user information
-let data = [
+let userDataBase = [
     {
-        name: 'Mazvita',
+        username: 'Mazvita',
         password: "secret",
         access: ["a"],
     },
     {
-        name: 'Meagan',
+        username: 'Meagan',
         password: 'secret',
         access: ["a", "b"]
     },
     {
-        name: "Kabelo",
+        username: "Kabelo",
         password: "secret",
         access: ["b", "c"]
     }
 ]
 
-//================HANDLE POST REQUESTS TO THE '/login' ENDPOINT===========
-app.post('/login', (req, res) => {
-    const auth = req.headers['authorization']
-    const token = auth
+//===============LOGIN ROUTE=================
+// Handles user authentication and generates JWT token
 
-    if (data.name) {
-        payload = {
-            name: data.name,
-            admin: true
+app.post('/login',(req, res) => {
+    // Extract username and password from the request body
+    const userLogin = req.body.username
+    const psw = req.body.password
+
+    // Find the index of the user in the database
+    const userIndex = userDataBase.findIndex(index => index.username === userLogin);
+
+    // Conditional rendering to check if the user exists and the password matches
+    if (userIndex > -1 && psw === userDataBase[userIndex].password) {
+        // If authentication is successful, create a payload for the JWT token
+        let payload = {
+            'name': userLogin,
+            'access': userDataBase[userIndex].access
         }
-        const token = jwt.sign(JSON.stringify(payload), 'jwt-secret',
-            { algorithm: 'HS256' })
-        res.send({ 'token': token })
-    }
+        // Sign the payload to generate the JWT token
+        const token = jwt.sign(JSON.stringify(payload), 'jwt-secret', {
+            algorithm: 'HS256'//algorithm for signing the token
+        })
+        // Send the generated token as a response
+        res.send({
+            'token': token
+        })
+    } 
     else {
-        res.status(403).send({ 'err': 'incorrect login!' })
+        // If authentication fails (user not found or incorrect password), send a 403 Forbidden status
+        res.status(403).send(
+            { 'err': 'incorrect login!' }
+        )
     }
 })
 
-// =============PROTECTED ROUTES=====================
-// Route accessible to users with the name 'Mazvita' or 'Meagan'
+//===============PROTECTED ROUTES=================
+
+// Route accessible to users with access 'a'
 app.get('/a', (req, res) => {
+    // Extract the JWT token from the Authorization header
+    const token = req.headers['authorization'].split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, 'jwt.secret');
+        //The jwt.verify() is used verify the authenticity of the JWT token. 
+        //It checks the signature and expiration of the token using the secret key ('jwt.secret').
 
-    const auth = req.headers['authorization']
-    const token = auth
-    if (data.name === 'Mazita' || data.name === 'Meagan') {
-        payload = {
-            name: data.name,
-            admin: true
+        const accessIndex = decoded.access.indexOf('a');
+
+        if(accessIndex > -1){
+            res.send(
+                { 'msg': 'success' }
+            )
         }
-        const token = jwt.sign(JSON.stringify(payload), 'jwt-secret',
-            { algorithm: 'HS256' })
-        res.send({ 'token': token })
-    }
-    else {
-        
-        res.status(403).json({ error: 'access denied' })
-    }
-})
+        else { 
+            res.status(403).json({ error: 'access denied' }) 
+            //If the access index is -1 (meaning 'a' is not present), send a 403 Forbidden status with an error message indicating access denial.
+             }
 
+    } catch (error) {
+        // If token verification fails, a 401 status code (Unauthorized) is returned
+        //401 Unauthorized response status code indicates that the client request has not been completed because it lacks valid authentication credentials for the requested resource.
+        res.sendStatus(401)
+    }
+
+});
 //Route accessible to users with the name 'Meagan' or 'Kabelo'
 app.get('/b', (req, res) => {
+    const token = req.headers['authorization'].split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, 'jwt.secret');
 
-    const auth = req.headers['authorization']
-    const token = auth
-    
-    if (data.name === 'Meagan' || data.name === 'Kabelo') {
-        payload = {
-            name: data.name,
-            admin: true
+        const accessIndex = decoded.access.indexOf('b');
+
+        if (accessIndex > -1) {
+            res.send(
+                { 'msg': 'success' }
+            )
         }
-        const token = jwt.sign(JSON.stringify(payload), 'jwt-secret',
-            { algorithm: 'HS256' })
-        res.send({ 'token': token })
+        else { res.status(403).json({ error: 'access denied' }) }
+
+    } catch (error) {
+                // If token verification fails, a 401 status code (Unauthorized) is returned
+        res.sendStatus(401)
     }
-    else {
-        // If access is denied, return an error response
-        res.status(403).json({ error: 'access denied' })
-    }
-})
+
+});
 
 //Route accessible only to users with the name 'Kabelo'
 app.get('/c', (req, res) => {
+    const token = req.headers['authorization'].split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, 'jwt.secret');
 
-    const auth = req.headers['authorization']
-    const token = auth
-    if (data.name === 'Kabelo') {
-        payload = {
-            name: data.name,
-            admin: true
+        const accessIndex = decoded.access.indexOf('c');
+
+        if (accessIndex > -1) {
+            res.send(
+                { 'msg': 'success' }
+            )
         }
-        const token = jwt.sign(JSON.stringify(payload), 'jwt-secret',
-            { algorithm: 'HS256' })
-        res.send({ 'token': token })
+        else { 
+            res.status(403).json({ error: 'access denied' })
+            //If the access index is -1 (meaning 'a' is not present), send a 403 Forbidden status with an error message indicating access denial.
+             }
+
+    } catch (error) {
+                        // If token verification fails, a 401 status code (Unauthorized) is returned
+
+        res.sendStatus(401)
     }
-    else {
-        res.status(403).json({ error: 'access denied' })
-    }
-})
+
+});
+
 
 //====================START THE SERVER===================
 // Start the server and listen on the specified port
