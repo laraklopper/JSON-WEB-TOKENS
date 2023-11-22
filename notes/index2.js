e5126e9a58b5d699a0795bc22796c28803277e4a
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-22 12:03:48 +0200

    Update index2.js

--- notes/index2.js
+++ notes/index2.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 8000;
+const express = require('express');// Import Express Web framework for handling HTTP requests
+const jwt = require('jsonwebtoken');// JSON Web Token library
+const bodyParser = require('body-parser');// Middleware for parsing request bodies
+const app = express();// Create an Express application
+const port = 8000;// Define the port for the server to listen on
 
+//==========Middleware setup================
+// Configuring middleware to parse JSON data
 app.use(bodyParser.json());
 
+//======DATA CONTAINING USER INFORMATION============
 let data = [
     {
         name: 'Mazvita',
@@ -21,15 +24,18 @@ let data = [
     }
 ]
 
+//================HANDLE POST REQUESTS TO THE '/login' ENDPOINT===========
+// Handling POST requests to the /login endpoint
 app.post('/login', (req, res) => {
+    // Extracting the token from the request headers
     const auth = req.headers['authorization'];
     const token = auth;
 
-    // Fix: Check if the provided token is valid and corresponds to a user in the data array
+    // Verifying if the provided token corresponds to a user in the data array
     const user = data.find(user => jwt.verify(token, 'jwt-secret').name === user.name);
 
     if (user) {
-        // Fix: Use the user object found above to create the payload
+        // If the user is found, create a new token with additional payload
         const payload = {
             name: user.name,
             admin: true
@@ -37,15 +43,20 @@ app.post('/login', (req, res) => {
         const token = jwt.sign(payload, 'jwt-secret', { algorithm: 'HS256' });
         res.send({ 'token': token });
     } else {
+        // If the user is not found, return a 403 Forbidden status with an error message
         res.status(403).send({ 'err': 'incorrect login!' });
     }
 });
 
+// ============= PROTECTED ROUTES ======================
+
+// Authorization endpoints for /a, /b, and /c
 app.get('/a', (req, res) => {
+    // ... (similar structure for /b and /c)
     const auth = req.headers['authorization'];
     const token = auth;
 
-    // Fix: Use the token to find the corresponding user in the data array
+    // Use the token to find the corresponding user in the data array
     const user = data.find(user => jwt.verify(token, 'jwt-secret').name === user.name);
 
     if (user && (user.name === 'Mazvita' || user.name === 'Meagan')) {
@@ -60,6 +71,7 @@ app.get('/a', (req, res) => {
     }
 });
 
+// Route accessible to users with the name 'Meagan' or 'Kabelo'
 app.get('/b', (req, res) => {
     const auth = req.headers['authorization'];
     const token = auth;
@@ -96,4 +108,6 @@ app.get('/c', (req, res) => {
     }
 });
 
+===================START THE SERVER===================
+// Start the server and listen on the specified port
 app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
